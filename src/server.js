7775f33998dceb3547465cce8ceed213bc20657f
ahaelim <limahae1015@gmail.com>
2022-11-30 00:44:07 +0900

    닉네임 채팅

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import http from "http";
 import WebSocket from "ws";
 import express from "express";
-import { parse } from "path";
 // 여기서 express로는 views를 설정하고 render해주기만 함
 // 나머지는 웹소켓에서 일어나게함
 // app은 console.log를 실행하고 포트 3000을  listen함 localhost:3000
